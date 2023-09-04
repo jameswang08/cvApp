82b82f87ccd4dfd3bf88429a73532af984490d0f
James Wang <114769089+jameswang08@users.noreply.github.com>
2023-09-04 11:28:23 -0700

    Add button to remove experiences

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -18,11 +18,17 @@ function Experience(){
         setExp(expCopy);
     }
 
+    function removeExperience(job){
+        const newExp = exp.filter(thisJob => thisJob !== job);
+        setExp(newExp);
+    }
+
     return(
         <>
             <h1>Experience</h1>
             {exp.map((job, index) => 
                 <div key={job.id}>
+                    <button onClick={() => removeExperience(job)}>Remove</button>
                     <div className="field">
                         <label>Company: </label>
                         <input type="text" value={job.company} onChange={event => updateExperience(index, "company", event.target.value)}></input>
